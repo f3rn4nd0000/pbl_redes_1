37d6ff37599eabfa2eb27807316c4fbd6446c89d
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-03-21 09:13:41 -0300

    feat: gerando fatura

--- server.py
+++ server.py
@@ -29,26 +29,40 @@ udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # tcp_client_socket.bind(TCP_ADDRESS)
 
+packets_received = []
+
 def handle_udp():
     print(f"Iniciando servidor UDP")
     while True:
         data, addr = my_udp_server.recvfrom(BUFFERSIZE)
         print(f"Recebendo dados do dispositivo: {addr}")
         print(f"Dados = {data}")
+        packets_received.append(data.decode('utf-8'))
         return data.decode('utf-8')
     
-def get_consumption(udp_data):
+def get_consumption_rate(udp_data):
     return json.loads(udp_data).get('taxa_consumo')
 
-def get_consumption(udp_data):
-    return json.loads(udp_data).get('consumo_total')
+def get_total_consumption(udp_data):
+    total_consumption_value = str(json.loads(udp_data).get('consumo_total')).split('KWh')[0]
+    return total_consumption_value
 
 def get_device_id(udp_data):
     return json.loads(udp_data).get('num_contrato')
 
+def get_day_time(udp_data):
+    return json.loads(udp_data).get('dia_horario')
+
 def get_request_param(request):
-    print(f'path: {request.split()[1]}')
+    print(f'path: {(request.split()[1]).decode("utf-8")}')
     return (request.split()[1]).decode('utf-8')
+
+def generate_bill():
+    message = f"""
+        FATURA CORRESPONDENTE AO PERIODO DE \n{get_day_time(packets_received[0])}\nA \n{get_day_time(packets_received[len(packets_received)-1])}
+        VALOR DA FATURA = R$ {int(get_total_consumption(packets_received[len(packets_received)-1])) - int(get_total_consumption(packets_received[0]))}
+    """
+    return message
 
 # def parse_tcp_request(request):
 #     return (str(request.split('GET /')[1]).split(' HTTP/1.1')[0])
@@ -56,16 +70,24 @@ def get_request_param(request):
 def handle_tcp():
     print(f"Iniciando servidor TCP na porta {TCP_PORT}")
     while True:
-        message = handle_udp()
-        num_contrato = get_device_id(message)
+        udp_data = handle_udp()
+        message = f"""
+        consumo instantaneo (KWh): {get_consumption_rate(udp_data)}
+        consumo total (KW): {get_total_consumption(udp_data)}
+        data e horario: {get_day_time(udp_data)}
+        """
+        num_contrato = get_device_id(udp_data)
         print('NUM. DO CONTRATO')
         print(num_contrato)
         print('dados recebidos do dispositivo UDP:')
-        print(message)
+        print(udp_data)
         conn, addr = my_tcp_server.accept()
         request = conn.recv(BUFFERSIZE)
         print('param do REQUEST')
         print(get_request_param(request))
+        url_path = get_request_param(request)
+        print('URL PATH')
+        print(url_path)
         tcp_user_id = get_request_param(request).split('/')[1]
         print("TCP USER ID!!!")
         print(tcp_user_id)
@@ -76,6 +98,10 @@ def handle_tcp():
         print('-----')
         print(request)
         if (tcp_user_id == num_contrato):
+            # conn.send(HTTP_RESPONSE.format(len(message), message).encode('utf-8'))
+            if url_path.endswith('/fatura'):
+                print('ENTREI NO IF DE URL PATH')
+                message = generate_bill()
             conn.send(HTTP_RESPONSE.format(len(message), message).encode('utf-8'))
         else:
             message = 'Esse usuario nao foi encontrado!!!'
